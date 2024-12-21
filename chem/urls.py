171139f4c5d84986217e253c2a8ce1122445b115
KBiR200 <73227990+KBiR200@users.noreply.github.com>
2024-12-21 15:36:40 +0300

    fixed a bug

--- chem/urls.py
+++ chem/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for chem project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -39,7 +23,7 @@ urlpatterns = [
     path('task/<int:pk>/submit/', reports.views.submit_task, name='submit_task'),
     
     # ''' reports '''
-    path('report/new/', reports.views.new_report, name='new_report'),
+    path('report/<int:task_id>/new/', reports.views.new_report, name='new_report'),
     path('report/<int:pk>/', reports.views.update_report, name='report'),
     path('report/<int:pk>/save/', reports.views.save_record, name='save_record'),
     path('report/<int:pk>/data/', reports.views.show_report, name='show_report'),
